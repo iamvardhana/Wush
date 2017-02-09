f916775677dc4ff229130ec23e2252749cb73fdd
Terence411 <loboterence4@gmail.com>
2017-02-09 21:43:16 +0530

    Update basic_structure.cpp

--- basic_structure.cpp
+++ basic_structure.cpp
@@ -1,28 +1,26 @@
 #include<iostream>
 using namespace std;
-
-struct example{
-int x;
-float y;
-char z;
-string s;
+struct example
+{ int x;
+  float y;
+  char z; 
+  string s;
 };
-
-int main(){
- example el1;
- cout<<"\nThis is an example for a structure.\n";
- cout<<"\nEnter an integer>";
- cin>>el1.x;
- cout<<"\nEnter a float value>";
- cin>>el1.y;
- cout<<"\nEnter a character>";
- cin>>el1.z;
- cout<<"\nEnter a string>";
- cin>>el1.s;
- cout<<"\n\nThe elements of the structure are:\n";
- cout<<"\n\ninteger>"<<el1.x;
- cout<<"\n\nfloat value>"<<el1.y;
- cout<<"\n\ncharacter>"<<el1.z;
- cout<<"\n\nstring>"<<el1.s<<endl;
-return 0;
+int main()
+{ example el1;
+  cout<<"\nThis is an example for a structure\n";
+  cout<<"\nEnter an integer\n";
+  cin>>el1.x;
+  cout<<"\nEnter a float value\n";
+  cin>>el1.y;
+  cout<<"\nEnter a character\n";
+  cin>>el1.z;
+  cout<<"\nEnter a string\n";
+  cin>>el1.s;
+  cout<<"\n\nThe elements of the structure are:\n";
+  cout<<"\nInteger value : "<<el1.x;
+  cout<<"\nFloat value : "<<el1.y;
+  cout<<"\nCharacter value : "<<el1.z;
+  cout<<"\nString value : "<<el1.s;
+ return 0;
 }
